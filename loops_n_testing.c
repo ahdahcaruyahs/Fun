1ff5307d75e46ba502180cf701c5fd2c2eb3eb23
LMXAB3KS <84719688+LMXAB3KS@users.noreply.github.com>
2023-12-10 12:51:12 +0530

    Create loops_n_testing.c

---
+++ loops_n_testing.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include<stdbool.h> //for bool
+
+int main() {
+    
+    while(1){
+        printf("true");
+    }
+    
+    for(;;){
+        printf("hello");
+    }
+    
+    while(true){
+        printf("hello from stdbool!");
+    }
+    
+    return 0;
+}
